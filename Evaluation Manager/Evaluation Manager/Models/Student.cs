7bfc69e083019a4fae17edbb630109c2350d995d
Robert Domgjonaj <rdomgjona21@foi.hr>
2023-05-12 11:10:54 +0200

    Create abstract class Person aswell as classes Teacher and Student

---
+++ Evaluation Manager/Evaluation Manager/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models
+{
+    internal class Student
+    {
+    }
+}
